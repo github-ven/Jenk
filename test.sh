79a25e4945f201c01d6016df92789b1e5ccb25ff
GIT-Admin <venkat.h87@gmail.com>
2021-05-08 18:40:42 +0530

    latest commit

--- test.sh
+++ test.sh
@@ -5,9 +5,5 @@ echo "third commit"
 echo "Fourth commit"
 echo "Fifth commit"
 echo "Sixth commit"
-echo "Eight Commit"
-<<<<<<< HEAD
 echo :seventh commit"
-=======
 echo "Eight Commit"
->>>>>>> 814dfc436f23d81403c74e7ffa2d6e755d19670c
