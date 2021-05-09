9212521db11c1a7f7022a16f7c1ce718d7ccd4a9
GIT-Admin <venkat.h87@gmail.com>
2021-05-09 17:28:04 +0530

    added remove file in tran_discover

--- tran_discover.sh
+++ tran_discover.sh
@@ -4,3 +4,5 @@ service_id="IHS-Service"
 #mysql --defaults-extra-file=./conf.properties appsone -se "select name from view_transaction_service;" > transactions.txt
 
 mysql --defaults-extra-file=./conf.properties appsone -se "select name from view_transaction_service where serviceName='$service_id';" > transaction_service.txt
+
+rm -rf transaction_service.txt
