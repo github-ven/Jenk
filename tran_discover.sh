36c23d0a6164024d9b52b8f85d5d411fabab3d62
GIT-Admin <venkat.h87@gmail.com>
2021-05-09 16:14:19 +0530

    new scripts added

---
+++ tran_discover.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+service_id="IHS-Service"
+
+#mysql --defaults-extra-file=./conf.properties appsone -se "select name from view_transaction_service;" > transactions.txt
+
+mysql --defaults-extra-file=./conf.properties appsone -se "select name from view_transaction_service where serviceName='$service_id';" > transaction_service.txt
